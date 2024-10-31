8a2ce2693beac82eb83df0370ee9369066bd4d5f
MusefulElias <eliasabrahamemail@gmail.com>
2024-10-31 02:09:03 +0000

    修复A01的bug

--- 1031/A01_Load/main.c
+++ 1031/A01_Load/main.c
@@ -4,12 +4,12 @@
 int main(int argc, char *argv[])
 {
     int count = 0;
-    int all = 100;
+    int all = 1000;
     while (count != all + 1)
     {
         printf("Loading: (%i/%i).\r", count, all);
         ++count;
-        sleep(1);
+    	usleep(10000);
     }
     puts("");
     puts("Done!");
